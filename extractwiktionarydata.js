28ad7442d209858b3f95bd1db300039315ae06e0
Blair McKenzie <shib71@gmail.com>
2014-04-25 09:12:53 +1000

    now

--- extractwiktionarydata.js
+++ extractwiktionarydata.js
@@ -2,11 +2,14 @@ var program = require('commander');
 var fs = require('fs');
 var util = require('util');
 var path = require('path');
+var CommandReadable = require("./lib/commandreadable.js");
 var PageSplitter = require('./lib/wikipagesplitter');
 var Filter = require('./lib/filter');
 var XMLParser = require('./lib/xmlparser');
 var ParseLatinInfo = require('./lib/parselatininfo');
+var JSONWriter = require("./lib/jsonwriter");
 var sprintf = require("sprintf").sprintf;
+var colors = require("colors");
 
 program.version("0.0.1")
     .option("-e, --extract <file>","Wiktionary extract file")
@@ -20,42 +23,73 @@ var tenses = [];
 
 var misfireLog = fs.createWriteStream(path.join(program.outputdir,"misfire.log"),{ encoding:"utf8" });
 var processedLog = fs.createWriteStream(path.join(program.outputdir,"processed.log"),{ encoding:"utf8" });
-var count = 0, sections = [], words = fs.createReadStream(program.extract,{ encoding:"utf8" })
+var processedData = fs.createWriteStream(path.join(program.outputdir,"wiktionary_data.json"),{ encoding:"utf8" });
+var count = 0, file = "";
+
+if (program.extract.search(/\.7z$/) > -1)
+	file = fs.createReadStream(program.extract,{ encoding:"utf8" });
+else
+	file = new CommandReadable({ debug:true, command:"7z", args:["e","-so",program.extract] });
+
+var words = file
 	.pipe(new PageSplitter())
 	.pipe(new Filter(function(page){ return page.search(/(^|[^=])==Latin==[^=]/i) > -1 }))
 	.pipe(new XMLParser())
 	.pipe(new Filter(function(result){ return result.page.title[0].search(/^\w+$/) > -1}))
 	.pipe(new ParseLatinInfo())
 	.on("misfire",function(info){
-		if (["fabella","y","be","comma","Syria","Jupiter","Macedonia","el","pupa","anterior","Abdera","er"].indexOf(info.page)===-1 && ["word"].indexOf(info.type)===-1) 
-			console.log(JSON.stringify(info));
-		
-		misfireLog.write(JSON.stringify(info)+"\n");
-	})
-	.on("readable",function(){
-		var page = {};
-		
-		while (null !== (page = this.read())){if (page.words.length && page.words[0].tense && tenses.indexOf(page.words[0].tense > -1)) tenses.push(page.words[0].tense);
-			count += 1;
-			
-			processedLog.write(sprintf(
-				"%20s: %3s words, %3s inflections\n",
-				page.word,
-				page.words.length, 
-				page.inflections.length
-			));
-			
+		if (info.type === "word"){
 			console.log(sprintf(
-				"%20s: %3s words, %3s inflections",
-				page.word,
-				page.words.length, 
-				page.inflections.length
-			));
+				"           %20s %15s %14s %3s",
+				info.word,
+				"N/A", 
+				info.subsection.toLowerCase(), 
+				0
+			).red);
+			console.log(info.text);
+			file.close();
+		}
+		if (info.type === "definition"){
+			console.log(sprintf(
+				"           %20s %15s %14s %3s",
+				info.word,
+				info.results.pos, 
+				info.results.type, 
+				info.results.definitions ? info.results.definitions.length : 0
+			).red);
+			console.log(info.history);
+			file.close();
+		}
+		if (false && info.type === "subsection"){
+			console.log(sprintf(
+				"           %20s %15s %14s %3s",
+				info.word,
+				"N/A", 
+				info.subsection.toLowerCase(), 
+				0
+			).red);
+			console.log(info.text);
+			file.close();
 		}
 	})
-	.on("end",function(){
-		console.log("parsed "+count+" pages");
-		console.log("found sections: "+sections.join(", "));
-		misfireLog.end();
-		processedLog.end();
-	});
+	.on("word",function(item){
+		console.log(sprintf(
+			"%10s %20s %15s %14s %3s",
+			++count,
+			item.word,
+			item.pos, 
+			item.type,
+			item.definitions ? item.definitions.length : 0
+		));
+	})
+	.pipe(new JSONWriter())
+	.pipe(processedData);
+
+file.on("close",function(){
+	console.log("parsed "+count+" words");
+	processedData.write("\n]\n");
+	
+	misfireLog.end();
+	processedLog.end();
+	processedData.end();
+});
